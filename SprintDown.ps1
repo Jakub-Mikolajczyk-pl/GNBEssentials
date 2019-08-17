24fbbac1c5fd3053a11f1bf9d8788c60f1b2e961
Xupi <jakubmikolajczyk@poczta.fm>
2019-08-17 19:53:10 +0200

    Little script for automatic event creation in outlook
    This script automate the boring stuff which was writing email to all testers and developers working on common workspace, to alert them about incoming installation, that could interrupt their workflow. This script creates event in their calendar, and alert them near the time of installation.

---
+++ SprintDown.ps1
@@ -0,0 +1,26 @@
+﻿$ol = New-Object -ComObject Outlook.Application
+$meeting = $ol.CreateItem('olAppointmentItem')
+$meeting.Subject = 'Niedostępność środowiska Sprint BPMS'
+$meeting.Body = ''
+$meeting.Location = 'Virtual'
+$meeting.ReminderSet = $true
+$meeting.Importance = 1
+$meeting.MeetingStatus = [Microsoft.Office.Interop.Outlook.OlMeetingStatus]::olMeeting
+$meeting.Recipients.Add('SomeCorporateMail@Domain.pl')
+$meeting.Recipients.Add('AnotherCorporateMail@Domain.pl')
+$meeting.ReminderMinutesBeforeStart = 10
+$meeting.Start = [datetime]::Now.AddMinutes(10)
+$meeting.Duration = 15
+$meeting.Send()
+
+#Start-Sleep -s 30
+
+#$mail = $ol.CreateItem('olMailItem')
+#$mail.Subject = 'sss'
+#$mail.Body = 'Test'
+#$mail.To="YourCorporateMail@domain.pl"
+#$mail.Send()
+
+$ol.Quit() 
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($ol) | Out-Null
+#Read-Host -Prompt "Press Enter to exit"
