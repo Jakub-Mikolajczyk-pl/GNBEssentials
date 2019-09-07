1cfaa0baba5f477a341d98bf66f9fc99abbba614
Xupi <jakubmikolajczyk@poczta.fm>
2019-09-07 23:49:29 +0200

    wip - zapis ustawien parametrów

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,29 +1,39 @@
+var settings = {
+  jenkins_address: "",
+  docker_address:  "",
+  propertyFileName: ""
+};
+
+function getSettings(settings) {
+  console.log(settings);
+   for (var propt in settings) {
+    chrome.storage.sync.get(settings[propt], function(result) {
+      console.log(result);
+      
+      if(Object.entries(result).length !== 0) {
+        console.log(result.seetings[propt]);
+        document.getElementById(setting[propt]).value = result.seetings[propt];
+      }
+    });
+     }
+};
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("saveButton").addEventListener('click', function() {saveSettings()} );
+    document.getElementById("saveButton").addEventListener('click', function() {saveSettings(settings)} );
+    chrome.storage.sync.onChanged.addListener(getSettings(settings));
 });
 
-function saveSettings() {
-  var settings = {
-    jenkins_address: document.getElementById("jenkins_address").value,
-    docker_address: document.getElementById("docker_address").value,
-    propertyFileName: document.getElementById("propertyFileName").value
-  };
+function saveSettings(settings) {
+  settings.jenkins_address = document.getElementById("jenkins_address").value;
+  settings.docker_address = document.getElementById("docker_address").value;
+  settings.propertyFileName = document.getElementById("propertyFileName").value;
 
   console.log(settings);
 
-  for (const [key, value] of Object.entries(settings)) {
-    chrome.storage.local.set({key: value});
-    console.log('zapisuje');
-    }
-
-    var jenkins_address = chrome.storage.local.get('jenkins_address', function(data) {
-        if (chrome.runtime.lastError) {
-            console.log('error');
-            return; 
-        }
-        console.log(data);
-        return data;
+  
+    chrome.storage.sync.set(settings, function() {
+      console.log('Settings saved');
     });
+    
        
-    console.log(jenkins_address);
 };
